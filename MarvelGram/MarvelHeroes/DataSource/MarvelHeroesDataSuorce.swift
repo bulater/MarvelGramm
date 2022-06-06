52914e0e93216c1e04753a5613edb2c339db0230
Bulat Sagdiev <com.BulatSagdiev>
2022-06-06 12:37:12 +0300

    added dataSource

---
+++ MarvelGram/MarvelHeroes/DataSource/MarvelHeroesDataSuorce.swift
@@ -0,0 +1,12 @@
+//
+//  MarvelHeroesDataSuorce.swift
+//  MarvelGram
+//
+//  Created by Булат Сагдиев on 06.06.2022.
+//
+
+import Foundation
+
+struct MarvelHeroesDataSuorce {
+    var marvelHeroViewModels: [MarvelHeroViewModel] = []
+}
